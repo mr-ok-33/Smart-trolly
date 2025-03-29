a1f00f4cc2c62172913ec211d4ca0b61681104ea
Omkar Sathe <satheomkar71@gmail.com>
2025-03-29 19:49:15 +0530

    Save change

--- src/Component/UI/Reviews.jsx
+++ src/Component/UI/Reviews.jsx
@@ -26,6 +26,38 @@ const Reviews = () => {
                 </div>
               </div>
             </div>
+            <div className="testimonial-card">
+              <div className="stars-date">
+                <div className="stars">⭐⭐⭐⭐⭐</div>
+                <div className="date">10th Feb, 2023</div>
+              </div>
+              <p className="testimonial-text">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore.
+              </p>
+              <div className="user-info">
+                <img src={profileImg} alt="User Image" />
+                <div>
+                  <h3>Ray Robertson</h3>
+                  <p>CEO Company</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial-card">
+              <div className="stars-date">
+                <div className="stars">⭐⭐⭐⭐⭐</div>
+                <div className="date">10th Feb, 2023</div>
+              </div>
+              <p className="testimonial-text">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore.
+              </p>
+              <div className="user-info">
+                <img src={profileImg} alt="User Image" />
+                <div>
+                  <h3>Ray Robertson</h3>
+                  <p>CEO Company</p>
+                </div>
+              </div>
+            </div>
 
             <div className="testimonial-card">
               <div className="stars-date">
